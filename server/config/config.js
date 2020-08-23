c1014a9f96f83a1b3e2f906f108930800f2f0111
KS <ksanchez@deipe.com>
2020-08-23 17:05:47 +0100

    Vaults

--- server/config/config.js
+++ server/config/config.js
@@ -3,14 +3,11 @@ process.env.PORT = process.env.PORT || 3000;
 //BD-MONGO - Heroku sabe si estamos en pro con esta variable
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-console.log(process.env.NODE_ENV);
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongodb-admin:fdnTx9ZszS7USmVg@cluster0.nz1ak.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
-console.log(urlDB);
 
 process.env.URLDB = urlDB;
